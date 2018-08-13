c6a010d61b22ecf98787a3e8643dbb9763c24bb5
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-13 19:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes={}
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({},object,{key,value})
+  return Object.assign({},object,{[key]:value})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
